875fb34e803968fc14369a0d65596e6c9e048d75
Jimmy Schementi <jschementi@gmail.com>
2011-03-04 22:20:06 -0500

    Add simple example "IronPythonTestProject"

---
+++ IronPythonTestProject/test_python.py
@@ -0,0 +1,12 @@
+﻿import clr
+clr.AddReference("Microsoft.VisualStudio.QualityTools.UnitTestFramework")
+from Microsoft.VisualStudio.TestTools.UnitTesting import Assert
+
+class TestFoo(object):
+    def test_should_pass(self):
+        Assert.IsTrue(True)
+    def test_should_pass2(self):
+        Assert.IsTrue(True)
+    def test_should_fail(self):
+        Assert.IsTrue(False)
+    
